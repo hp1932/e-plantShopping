7c0fa4f19cf4753c33c97324590795f65711a1ed
H <>
2025-02-07 19:08:36 -0500

    Task 3, partial. On step 7

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    cart.forEach((item) => {
+        totalCost += item.cost * item.quantity;
+    });
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+    e();
   };
 
-
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}))
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity <= 1)
+    {
+        dispatch(removeItem(item))
+    }
+    else
+    {
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}))
+    }
+
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let numInCart = cart.items[item.name] ? cart.items[item.name].quantity : 0;
+    return item.cost * numInCart;
   };
 
   return (
